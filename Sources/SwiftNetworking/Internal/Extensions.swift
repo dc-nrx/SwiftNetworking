b55c8f3f0bc65d294e812b6084e4ca071970aa00
Dmytro Chapovskyi <dc_dc@protonmail.com>
2023-04-21 10:49:57 +0200

    error == operator fix

--- Sources/SwiftNetworking/Internal/Extensions.swift
+++ Sources/SwiftNetworking/Internal/Extensions.swift
@@ -19,7 +19,7 @@ internal func == (lhs: Error, rhs: Error) -> Bool {
 	guard type(of: lhs) == type(of: rhs) else { return false }
 	let error1 = lhs as NSError
 	let error2 = rhs as NSError
-	return error1.domain == error2.domain && error1.code == error2.code && "\(lhs)" == "\(rhs)"
+	return error1.domain == error2.domain && error1.code == error2.code
 }
 
 internal extension Equatable where Self : Error {
